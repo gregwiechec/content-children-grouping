8531cbe10b160c853b7cb0ad450b995b2528bc1f
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2021-07-10 00:51:15 +0200

    Add content generator

---
+++ src/AlloySample/Plugins/ArticlesGenerator.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ArticlesGenerator.aspx.cs" Inherits="AlloySample.Plugins.ArticlesGenerator" %>
+<%@ Register TagPrefix="EPiServerUI" Namespace="EPiServer.UI.WebControls" Assembly="EPiServer.UI, Version=11.25.0.0, Culture=neutral, PublicKeyToken=8fe83dea738b45b7" %>
+
+<asp:Content runat="server" ContentPlaceHolderID="MainRegion">
+    <div class="epi-formArea">
+        <div class="epi-size20">
+            <div>
+                <asp:Label runat="server" AssociatedControlID="ArticlesContainer" Translate="<%$ Resources: EPiServer, admin.siteinformationedit.startpage%>" />
+                <EPiServer:InputPageReference Style="display: inline;" ID="ArticlesContainer" AutoPostBack="false" runat="server"/>
+            </div>
+            <div>
+                <asp:Label AssociatedControlID="NumberOfArticles" runat="server" >Number of articles</asp:Label>
+                <asp:TextBox ID="NumberOfArticles" runat="server" MaxLength="4" Width="100" Text="100" />
+                <asp:RangeValidator ID="MaxNumberOfArticlesValidator" runat="server" ControlToValidate="NumberOfArticles" Type="Integer" MinimumValue="1" MaximumValue="10000" Display="Dynamic" Text="*" ErrorMessage="Number of articles should be between 1 and 10000" />
+                <asp:RequiredFieldValidator runat="server" ControlToValidate="NumberOfArticles" Display="Dynamic" Text="*" ErrorMessage="Number of articles is required" />
+            </div>
+            <div class="epi-indent">
+                <EPiServerUI:ToolButton ID="GenerateButton" OnClick="GenerateButton_Click" runat="server" SkinID="Report" Text="Generate" />
+            </div>
+            <div>
+                <asp:Label ID="lblResult" runat="server" Visible="false">Content generated</asp:Label>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ src/AlloySample/Plugins/ArticlesGenerator.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Web;
+using AlloySample.Models.Pages;
+using EPiServer;
+using EPiServer.DataAccess;
+using EPiServer.PlugIn;
+using EPiServer.Security;
+using EPiServer.ServiceLocation;
+using EPiServer.Web;
+
+namespace AlloySample.Plugins
+{
+    [GuiPlugIn(DisplayName = "Generate articles",
+        Description = "Generate articles",
+        Area = EPiServer.PlugIn.PlugInArea.AdminMenu,
+        Url = "~/Plugins/ArticlesGenerator.aspx")]
+    public partial class ArticlesGenerator : EPiServer.Shell.WebForms.WebFormsBase
+    {
+        protected Injected<IContentRepository> _contentRepository;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            if (!PrincipalInfo.HasAdminAccess)
+            {
+                Locate.Advanced.GetInstance<IAccessDeniedHandler>()
+                    .AccessDenied(new HttpContextWrapper(HttpContext.Current));
+            }
+
+            SystemMessageContainer.Heading = "Generate articles";
+        }
+
+        protected void GenerateButton_Click(object sender, EventArgs e)
+        {
+            if (!Page.IsValid)
+            {
+                return;
+            }
+
+            var numberOfArticles = int.Parse(this.NumberOfArticles.Text);
+            var containerContentLink = ArticlesContainer.PageLink;
+
+            var now = DateTime.Now;
+
+            for (var i = 0; i < numberOfArticles; i++)
+            {
+                var index = i / ListOfAnimals.Animals.Length;
+
+                var articlePage = _contentRepository.Service.GetDefault<ArticlePage>(containerContentLink);
+                articlePage.Name = ListOfAnimals.Animals[i % ListOfAnimals.Animals.Length] +
+                                   (index == 0 ? "" : index.ToString());
+                articlePage.Created = now.AddMonths(i % 20);
+                _contentRepository.Service.Save(articlePage, SaveAction.Publish, AccessLevel.NoAccess);
+            }
+
+            lblResult.Visible = true;
+        }
+    }
+}
