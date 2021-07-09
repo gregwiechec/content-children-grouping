4a5d9b05c82b4fe66e97ba10afb22e2be0127a66
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2021-07-10 01:18:44 +0200

    Add configuration plugin

---
+++ src/AlloySample/modules/_protected/content-children-grouping/Plugins/GroupingConfig.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GroupingConfig.aspx.cs" Inherits="ContentChildrenGrouping.Plugin.GroupingConfig" %>
+<%@ Import Namespace="EPiServer.Framework.Modules.Internal" %>
+
+<asp:Content runat="server" ContentPlaceHolderID="MainRegion">
+    
+    <%= ModuleResourceResolver.Instance.ResolveClientPath("content-children-grouping", "grouping-config.js") %>
+    
+    
+    <div class="epi-formArea">
+        <div class="epi-size20">
+            aaa
+        </div>
+    </div>
+</asp:Content>
---
+++ src/ContentChildrenGrouping/Plugin/GroupingConfig.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web;
+using EPiServer.PlugIn;
+using EPiServer.Security;
+using EPiServer.Web;
+
+namespace ContentChildrenGrouping.Plugin
+{
+    [GuiPlugIn(DisplayName = "Configure content groups",
+        Description = "Configure content groups",
+        Area = EPiServer.PlugIn.PlugInArea.AdminMenu,
+        UrlFromModuleFolder = "Plugins/GroupingConfig.aspx")]
+    public partial class GroupingConfig : EPiServer.Shell.WebForms.WebFormsBase
+    {
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            if (!PrincipalInfo.HasAdminAccess)
+            {
+                Locate.Advanced.GetInstance<IAccessDeniedHandler>()
+                    .AccessDenied(new HttpContextWrapper(HttpContext.Current));
+            }
+
+            SystemMessageContainer.Heading = "Configure content groups";
+        }
+    }
+}
