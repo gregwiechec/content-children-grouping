7188c0ea27a50bc9100883c767b1dec039c2bc85
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2021-07-12 07:21:39 +0200

    use optimizely components

--- clientResources/src/configurations-list.tsx
+++ clientResources/src/configurations-list.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BlockList } from 'optimizely-oui';
+
+import "optimizely-oui/dist/styles.css";
 
 interface ConfigurationsListProps {
     items: any[];
@@ -6,8 +9,18 @@ interface ConfigurationsListProps {
 
 export const ConfigurationsList = ({ items }: ConfigurationsListProps) => {
     return (
-        <ul>
-            {items.map(x => <li>test</li>)}
-        </ul>
+        <BlockList hasBorder={ true } >
+            <BlockList.Category header="Header">
+                <BlockList.Item>
+                    Home
+                </BlockList.Item>
+                <BlockList.Item>
+                    Shopping Cart
+                </BlockList.Item>
+                <BlockList.Item>
+                    Order Confirmation
+                </BlockList.Item>
+            </BlockList.Category>
+        </BlockList>
     );
 }
