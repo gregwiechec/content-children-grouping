4130db57589c6cddad34e1f4feaaaf8410a6dd85
gregwiechec <gwiechec@o2.pl>
2021-10-07 09:22:25 +0200

    Select tree when click on vitual container item

--- src/AlloySample/modules/_protected/content-children-grouping/1.0.0/scripts/virtual-containers.js
+++ src/AlloySample/modules/_protected/content-children-grouping/1.0.0/scripts/virtual-containers.js
@@ -1,15 +1,18 @@
 define([
+	"dojo/Deferred",
 	"epi/shell/dnd/tree/multiDndSource",
 	"epi-cms/component/ContentNavigationTree",
 	"epi-cms/widget/ContentTree"
 ], function (
+	Deferred,
 	multiDndSource,
 	ContentNavigationTree,
 	ContentTree
 ) {
     return function(configuredContainerIds) {
 		var virtaulContainersProvider = "_VirtualContainers";
-		
+
+		// do not allow to select virtual containers
 		var originalOnMouseDown = multiDndSource.prototype.onMouseDown;
         multiDndSource.prototype.onMouseDown = function (e) {
 			if (this.current && this.current.item.contentLink.indexOf(virtaulContainersProvider) !== -1) {
@@ -20,6 +23,7 @@ define([
         };
         multiDndSource.prototype.onMouseDown.nom = "onMouseDown";
 
+		// do not allow to click on virtual container
 		var originalOnTreeNodeClicked = ContentNavigationTree.prototype._onTreeNodeClicked;
         ContentNavigationTree.prototype._onTreeNodeClicked = function (page) {
 			if (page && page.contentLink.indexOf(virtaulContainersProvider) !== -1) {
@@ -30,6 +34,7 @@ define([
         };
         ContentNavigationTree.prototype._onTreeNodeClicked.nom = "_onTreeNodeClicked";
 
+		// hide context menu for virtual container
 		var originalBuildNodeFromTemplate = ContentTree.prototype.buildNodeFromTemplate;
 		ContentTree.prototype.buildNodeFromTemplate = function (args) {
 			if (args.item && args.item.contentLink && args.item.contentLink.indexOf(virtaulContainersProvider) !== -1) {
@@ -38,7 +43,33 @@ define([
 
 			return originalBuildNodeFromTemplate.apply(this, arguments);
         };
-		ContentTree.prototype.buildNodeFromTemplate.nom = "originalBuildNodeFromTemplate";
+		ContentTree.prototype.buildNodeFromTemplate.nom = "buildNodeFromTemplate";
+
+		// update selected path and add virtual containers
+		var originalGetTreePath = ContentTree.prototype._getTargetPath;
+		ContentTree.prototype._getTargetPath = function (args) {
+			if (!this.selectedNode) {
+				return originalGetTreePath.apply(this, arguments);
+            }
+			var parent = this.selectedNode.getParent();
+			if (!parent || !parent.item || !parent.item.contentLink) {
+				return originalGetTreePath.apply(this, arguments);
+			}
+
+			// not a virtual container
+			if (parent.item.contentLink.indexOf(virtaulContainersProvider) === -1) {
+				return originalGetTreePath.apply(this, arguments);
+            }
+
+			var result = originalGetTreePath.apply(this, arguments);
+			var deferred = new Deferred();
+			result.then(function (targetPath) {
+				targetPath.splice(targetPath.length - 1, 0, parent.item.contentLink);
+				deferred.resolve(targetPath);
+			});
+			return deferred.promise;
+        };
+		ContentTree.prototype._getTargetPath.nom = "_getTargetPath";
 	}
 });
 
