a4f67af8e7ff744f20d8e1916e97f919e617d386
gregwiechec <gwiechec@o2.pl>
2021-10-20 13:43:13 +0200

    Allow to run scheduled job only when StructureUpdateEnabled is false

--- src/ContentChildrenGrouping.Core/ContentChildrenGroupingOptions.cs
+++ src/ContentChildrenGrouping.Core/ContentChildrenGroupingOptions.cs
@@ -9,28 +9,33 @@ namespace ContentChildrenGrouping.Core
     public class ContentChildrenGroupingOptions
     {
         /// <summary>
-        /// When true, then routing is globally enabled. Default true
+        /// When true, then routing is globally enabled.
+        /// Default true
         /// For complex scenarios, because of the performance it's better to implement custom PartialRouter.
         /// </summary>
         public bool RouterEnabled { get; set; } = true;
 
         /// <summary>
-        /// When true, then during content saving the structure is updated, default true
+        /// When true, then during content saving, the structure is updated.
+        /// Default true
         /// </summary>
         public bool StructureUpdateEnabled { get; set; } = false;
 
         /// <summary>
-        /// When true, then containers can be configured using admin plugin
+        /// When true, then containers can be configured using admin plugin.
+        /// Default false
         /// </summary>
         public bool DatabaseConfigurationsEnabled { get; set; } = false;
 
         /// <summary>
-        /// When true, then container icon is replaced with custom icon. Default true
+        /// When true, then container icon is replaced with custom icon.
+        /// Default true
         /// </summary>
         public bool CustomIconsEnabled { get; set; } = true;
 
         /// <summary>
-        /// When true, then search commands are available in page tree and assets. Default true
+        /// When true, then search commands are available in page tree and assets.
+        /// Default true
         /// </summary>
         public bool SearchCommandEnabled { get; set; } = true;
     }
